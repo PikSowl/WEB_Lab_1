25de6e30b80cc55694e19d4e11e257c7a95d18e8
PikSowl <kmazalov11@gmai.com>
2024-03-09 14:20:47 +0200

    2. Numbers, echoed some number according to TA

--- code/index.php
+++ code/index.php
@@ -2,10 +2,19 @@
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
 
-
-// Write your code here:
+echo "1.";
 $order =& $very_bad_unclear_name;
 $order .= ", a number six with extra dip";
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
-echo "\n";
+
+echo "\n2.\n";
+$lyuboye_imya = -1;
+echo "$lyuboye_imya\n";
+$lyubim_imenem = 1.1;
+echo "$lyubim_imenem\n";
+echo $lyuboye_imya+=13, "\n";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month, "\n";
+
